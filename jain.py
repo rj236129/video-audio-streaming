1ae2c5e99f3f34e4ad47d02b604096d1dc1596bf
rj236129 <ravijain236129@gmail.com>
2025-09-22 23:35:59 +0530

    Create jain.py

---
+++ jain.py
@@ -0,0 +1,30 @@
+import cv2
+import socket
+import pickle
+import struct
+
+HOST = 'PC_IP_ADDRESS'  # PC ka Wi-Fi IP
+PORT = 9999  # Video port same as server
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+data = b""
+payload_size = struct.calcsize("L")
+
+while True:
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Mobile View", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
